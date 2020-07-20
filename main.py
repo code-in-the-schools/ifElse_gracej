9db4be5ed15f8ae8bde06017bb2591df688df9d9
gracejoh <ganwynj@icloud.com>
2020-07-20 15:31:33 +0000

    ,

--- main.py
+++ main.py
@@ -19,16 +19,35 @@ for i in list1:
     print(i, ":", "vowel")
   else:
     print(i, ":", "consonant")
+  
 
 
 ###
-word = input(str("word: "))
 
-vowel = 1
-consonant = 1
+vowel = 0
+consonant = 0
 
 for x in list1:
   if (x == 'a' or x == 'e' or x == 'i' or x == 'o' or x == 'u' or x == 'A' or x == 'E' or x == 'I' or x == 'O' or x == 'U'):
-    print (vowel * x)
+    vowel = vowel + 1
   else:
-    print (consonant * x)
+    consonant = consonant +1
+print("vowels:", vowel)
+print("consonants:", consonant)
+
+###
+
+#input a word
+word2 = input(str("word: "))
+
+#convert the word to a list
+list2 = list(word2)
+
+#find if a vowel or consonant
+for i in list2:
+  if (i == "a" or i == "e" or i == "i" or i == "o" or i == "u" or i == "A" or i == "E" or i == "I" or i == "O" or i == "U"):
+    print(i, ":", "vowel")
+  elif (i != "a" or i != "e" or i != "i" or i != "o" or i != "u" or i != "A" or i != "E" or i != "I" or i != "O" or i != "U" or i != "!"):
+    print(i, ":", "consonant")
+  else:
+    print(i, ":", "special character")
